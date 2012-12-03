87fce69a87649f216ab3c561cf1008c0f493fd83
Franz Nieschalk <franz.nieschalk@gmail.com>
2012-12-03 17:41:53 +0100

    Visualisierung und YAML-Import von Links

---
+++ Flerdi_Editor_Alpha/js/network_elements/link.js
@@ -0,0 +1,30 @@
+/*
+ * Module link: represents a single link in the network
+ * Author: Flerdi Team, Franz Nieschalk
+ */
+ 
+ /*
+ * RequireJS module definition
+ */ 
+define (["jquery", "link_visualisation"], (function($, Link_Visualisation) {
+
+	/* constructor */
+	var Link = function(network_element, startNode, endNode) {
+	
+		console.log("creating link");
+		
+		this.startPosition = startNode.position || [Math.floor(Math.random() * 501), Math.floor(Math.random() * 501)];
+		//this.endPosition = endNode.position || [Math.floor(Math.random() * 501), Math.floor(Math.random() * 501)];
+		this.endPosition = [0, 0];
+
+		this.network_element = network_element;	
+		this.attributes = network_element.attributes;
+		this.id = network_element.attributes.id;
+		this.type = network_element.attributes.ne_type;
+		
+		this.visualisation = new Link_Visualisation(this.startPosition, this.endPosition, this.type);
+		this.visualisation.show();
+	} //constructor
+	
+	return Link;
+}));
