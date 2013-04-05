ec9cfc021f9d6ccdfb7e398b0194306c1da89865
Stefan <zimtis@t-online.de>
2013-04-05 17:31:51 +0200

    parsertest

--- backend/YAML_parser.js
+++ backend/YAML_parser.js
@@ -30,6 +30,7 @@ define (["jquery",'loadingWindow'],
 	Parser.loadFromText = function(_text,name, callback){
 		var loadingWindow = new LoadingWindow('\''+name+'\' wird geladen...');
 		var _this = this;
+		console.log(_text);
 		$.post("/backend/YAML_parser.php", { source: _text, type : 'text'}, function(json) {
 								console.log(json);
 								loadingWindow.close();
