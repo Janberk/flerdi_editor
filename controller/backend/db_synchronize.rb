f307d2ef5be159c77155ff0fbfede1667dc4e903
Corina Meyer <corina.meyer@gmx.net>
2013-06-26 08:13:45 +0200

    orm server side

--- controller/backend/db_synchronize.rb
+++ controller/backend/db_synchronize.rb
@@ -54,7 +54,9 @@ end
 post '/network_element/new' do
   ne = GraphLabel.find(params[:graph_label].to_i).network_elements.create(
     :identifier => params[:identifier],
-    :ne_type => params[:ne_type]
+    :ne_type => params[:ne_type],
+    :v_net_identifier => params[:v_net_identifier],
+    :alias => params[:alias]
   )
   ne.features.create(
     :avp_attribute => "position_x",
@@ -79,13 +81,15 @@ put '/network_element/:id' do
   else
     ne.update(
       :identifier => params[:identifier],
-      :ne_type => params[:ne_type]
+      :ne_type => params[:ne_type],
+      :v_net_identifier => params[:v_net_identifier],
+      :alias => params[:alias]
     )
   end
 end
 
 delete '/network_element/:id' do
-  NetworkElement.find(params[:id].to_i).delete_network_element()
+  NetworkElement.find(params[:id].to_i).delete_network_element
 end
 
 ##################################
@@ -93,15 +97,25 @@ end
 ##################################
 
 post '/network_interface/new' do
-  
+  ne = NetworkElement.find(params[:network_element].to_i)
+  ne.network_interfaces.create(
+    :identifier => params[:identifier],
+    :alias => params[:alias],
+    :ni_type => params[:ni_type]
+  ).to_json
 end
 
 put '/network_interface/:id' do
-  
+  NetworkInterface.update(
+    params[:id].to_i,
+    :identifier => params[:identifier],
+    :alias => params[:alias],
+    :ni_type => params[:ni_type]
+  )
 end
 
 delete '/network_interface/:id' do
-  
+  NetworkInterface.find(params[:id].to_i).delete_network_interface
 end
 
 #########################
@@ -109,15 +123,32 @@ end
 #########################
 
 post '/resource/new' do
-  
+  el = ""
+  if params[:target] == 'ni'
+    el = NetworkInterface.find(params[:id].to_i)
+  else
+    el = NetworkElement.find(params[:id].to_i)
+  end
+  el.resources.create(
+    :avp_attribute => params[:avp_attribute],
+    :value => params[:value].to_i,
+    :value_type => params[:value_type],
+    :is_request => params[:is_request].to_i
+  ).to_json
 end
 
 put '/resource/:id' do
-  
+  Resource.update(
+    params[:id].to_i,
+    :avp_attribute => params[:avp_attribute],
+    :value => params[:value].to_i,
+    :value_type => params[:value_type],
+    :is_request => params[:is_request].to_i
+  )
 end
 
 delete '/resource/:id' do
-  
+  Resource.find(params[:id].to_i).delete
 end
 
 ########################
@@ -125,7 +156,13 @@ end
 ########################
 
 post '/feature/new' do
-  NetworkElement.find(params[:network_element].to_i).features.create(
+  el = ""
+  if params[:target] == 'ni'
+    el = NetworkInterface.find(params[:id].to_i)
+  else
+    el = NetworkElement.find(params[:id].to_i)
+  end
+  el.features.create(
     :avp_attribute => params[:avp_attribute],
     :value => params[:value],
     :priority => params[:priority].to_i,
@@ -134,9 +171,46 @@ post '/feature/new' do
 end
 
 put '/feature/:id' do
-  
+  Feature.update(
+    params[:id].to_i,
+    :avp_attribute => params[:avp_attribute],
+    :value => params[:value],
+    :priority => params[:priority].to_i,
+    :is_request => params[:is_request].to_i
+  )
 end
 
 delete '/feature/:id' do
-  
+  Feature.find(params[:id].to_i).delete
+end
+
+
+#######################################
+# synchronize: provisioning_interface #
+#######################################
+
+post '/provisioning_interface/new' do
+  ne = NetworkElement.find(params[:network_element].to_i)
+  ne.provisioning_interface.create(
+    :adress => params[:adress],
+    :port => params[:port],
+    :username => params[:username],
+    :password => params[:password],
+    :protocol => params[:protocol]
+  ).to_json
+end
+
+put '/provisioning_interface/:id' do
+  ProvisioningInterface.update(
+    params[:id].to_i,
+    :adress => params[:adress],
+    :port => params[:port],
+    :username => params[:username],
+    :password => params[:password],
+    :protocol => params[:protocol]
+  )
+end
+
+delete '/provisioning_interface/:id' do
+  ProvisioningInterface.find(params[:id].to_i).delete_network_interface
 end
